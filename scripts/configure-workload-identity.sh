3e4223c695a92fcb8748f01fdbdd5924c0a87197
Matt Elgin <melgin@cloudbees.com>
2019-10-21 17:35:47 -0400

    remove procMount lines

--- scripts/configure-workload-identity.sh
+++ scripts/configure-workload-identity.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 # connect to cluster
-# gcloud container clusters get-credentials melgin-alpha
+gcloud container clusters get-credentials melgin-alpha
 
 # create GSA (if not already created)
-# gcloud iam service-accounts create img-gcr
+gcloud iam service-accounts create img-gcr
 
 # grant Storage Admin permissions to GSA
-# gcloud projects add-iam-policy-binding melgin \
-#   --member serviceAccount:img-gcr@melgin.iam.gserviceaccount.com \
-#   --role roles/storage.admin
+gcloud projects add-iam-policy-binding melgin \
+  --member serviceAccount:img-gcr@melgin.iam.gserviceaccount.com \
+  --role roles/storage.admin
 
 # create namespace
 kubectl create namespace img
--- scripts/create-alpha-cluster.sh
+++ scripts/create-alpha-cluster.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 gcloud beta container clusters create melgin-alpha \
+  --cluster-version 1.14.6-gke.2 \
   --enable-kubernetes-alpha \
   --enable-pod-security-policy \
-  --identity-namespace=melgin.svc.id.goog
-  --machine-type=n1-standard-2
+  --identity-namespace=melgin.svc.id.goog \
+  --machine-type=n1-standard-2 \
   --image-type=cos_containerd
